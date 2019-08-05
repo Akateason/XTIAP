1069a83331618ae02c2bea611ee3da6bee76a054
谢天宸 <xietianchen@shimo.im>
2019-08-05 16:05:29 +0800

    Initial Commit

---
+++ XTIAP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTIAP
+//
+//  Created by teason23 on 2019/8/5.
+//  Copyright © 2019 teason23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XTIAP/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XTIAP
+//
+//  Created by teason23 on 2019/8/5.
+//  Copyright © 2019 teason23. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
